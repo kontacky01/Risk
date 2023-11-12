0657b42c0c0ecb41f8a81be3856f1ada977cdbdf
kontacky01 <144738021+kontacky01@users.noreply.github.com>
2023-11-12 16:23:33 -0500

    Update GameEngine.h

--- src/GameEngine/GameEngine.h
+++ src/GameEngine/GameEngine.h
@@ -93,6 +93,10 @@ public:
  * This function checks if the currentState is winstate
 */
     bool isCurrentStateWinState();
+/**
+ * This function checks if the currentState is endstate
+*/
+    bool isCurrentStateEndState();
 
     int getPlayerNum() const;
 
